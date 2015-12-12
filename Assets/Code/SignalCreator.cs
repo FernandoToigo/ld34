95029305a9c43d12ec911357138e72d534a1b55f
Fernando <fmtoigo@gmail.com>
2015-12-12 17:39:25 -0200

    - Added coisas.

--- Assets/Code/SignalCreator.cs
+++ Assets/Code/SignalCreator.cs
@@ -7,17 +7,23 @@ public class SignalCreator : MonoBehaviour
     public GameObject SignalSourcePrefab;
     public GameObject SignalTargetPrefab;
 
-    private List<Signal> _signals;
+    private static List<Signal> _signals;
+    public static List<Signal> Signals
+    {
+        get { return _signals; }
+        set { _signals = value; }
+    }
+
     private float _newSignalTimer;
 
     void Start()
     {
-        _signals = new List<Signal>();
+        Signals = new List<Signal>();
     }
 
     void Update()
     {
-        _newSignalTimer -= Time.deltaTime;
+        //_newSignalTimer -= Time.deltaTime;
         if (_newSignalTimer <= 0.0f)
         {
             _newSignalTimer += 5.0f;
@@ -25,18 +31,19 @@ public class SignalCreator : MonoBehaviour
             var signal = CreateRandomSignal();
             var sourcePos = new Vector3(Mathf.Cos(signal.AngleSource), Mathf.Sin(signal.AngleSource), 0.0f) * 4.0f;
             var targetPos = new Vector3(Mathf.Cos(signal.AngleTarget), Mathf.Sin(signal.AngleTarget), 0.0f) * 4.0f;
-            Debug.DrawLine(new Vector3(), sourcePos, Color.red, 10.0f);
-            Debug.DrawLine(new Vector3(), targetPos, Color.green, 10.0f);
 
             var sourcePrefab = GameObject.Instantiate(SignalSourcePrefab);
+            sourcePrefab.GetComponent<SignalSource>().Signal = signal;
             sourcePrefab.transform.position = sourcePos;
             sourcePrefab.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, signal.AngleSource * Mathf.Rad2Deg);
+            signal.SourceGameObject = sourcePrefab;
 
             var targetPrefab = GameObject.Instantiate(SignalTargetPrefab);
             targetPrefab.transform.position = targetPos;
             targetPrefab.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, signal.AngleTarget * Mathf.Rad2Deg);
+            signal.TargetGameObject = targetPrefab;
 
-            _signals.Add(signal);
+            Signals.Add(signal);
         }
     }
 
@@ -69,5 +76,26 @@ public class Signal
     {
         get { return _angleTarget; }
         set { _angleTarget = value; }
+    }
+
+    private GameObject _sourceGameObject;
+    public GameObject SourceGameObject
+    {
+        get { return _sourceGameObject; }
+        set { _sourceGameObject = value; }
+    }
+
+    private GameObject _targetGameObject;
+    public GameObject TargetGameObject
+    {
+        get { return _targetGameObject; }
+        set { _targetGameObject = value; }
+    }
+
+    private int _totalData;
+    public int TotalData
+    {
+        get { return _totalData; }
+        set { _totalData = value; }
     }
 }
--- Assets/Code/SignalSource.cs
+++ Assets/Code/SignalSource.cs
@@ -5,46 +5,78 @@ using System.Collections.Generic;
 public class SignalSource : MonoBehaviour
 {
     public LayerMask WorldMask;
+    public LayerMask SignalTargetMask;
     public LayerMask MirrorMask;
     public GameObject ReflectionPrefab;
 
     private GameObject _satellite;
+    private GameObject _mainMirror;
     private GameObject _signalPos;
     private List<GameObject> _reflections;
+
+    private Signal _signal;
+    public Signal Signal
+    {
+        get { return _signal; }
+        set { _signal = value; }
+    }
 
     void Start()
     {
         _satellite = GameObject.Find("Satellite");
+        _mainMirror = _satellite.transform.FindChild("Mirror").gameObject;
         _signalPos = transform.FindChild("SignalPos").gameObject;
         _reflections = new List<GameObject>();
     }
-    
+
     void Update()
     {
-        //foreach (var reflection in _reflections)
-        //    GameObject.Destroy(reflection);
-        //_reflections.Clear();
+        foreach (var reflection in _reflections)
+            GameObject.Destroy(reflection);
+        _reflections.Clear();
 
-        //CreateReflections(_signalPos.transform.position, (_satellite.transform.position - _signalPos.transform.position).normalized);
+        CreateReflections(_signalPos.transform.position, (_mainMirror.transform.position - _signalPos.transform.position).normalized);
     }
 
     private void CreateReflections(Vector3 startPos, Vector3 direction)
     {
-        if (!Physics.Linecast(startPos, direction * 10.0f, WorldMask))
+        var dest = startPos + direction * 10.0f;
+        RaycastHit hit;
+        if (!Physics.Linecast(startPos, dest, out hit, WorldMask | SignalTargetMask))
         {
-            RaycastHit hit;
-            if (Physics.Linecast(startPos, startPos, out hit, MirrorMask))
+            if (Physics.Linecast(startPos, dest, out hit, MirrorMask))
             {
-                var reflection = GameObject.Instantiate(ReflectionPrefab);
-                var lineRenderer = reflection.GetComponent<LineRenderer>();
-                lineRenderer.SetPosition(0, _signalPos.transform.position);
-                lineRenderer.SetPosition(1, _satellite.transform.position);
+                AddReflection(startPos, hit.point);
 
                 var mirror = hit.collider.gameObject;
+                mirror.layer = LayerMask.NameToLayer("Default");
                 CreateReflections(hit.point, Vector3.Reflect(direction, mirror.transform.forward));
+                mirror.layer = LayerMask.NameToLayer("Mirror");
+            }
+            else
+                AddReflection(startPos, dest);
+        }
+        else
+        {
+            AddReflection(startPos, hit.point);
 
-                _reflections.Add(reflection);
+            if (1 << hit.collider.gameObject.layer == SignalTargetMask && _signal.TargetGameObject == hit.collider.gameObject)
+            {
+                //_signal.TotalData -= Time.deltaTime;
+                SignalCreator.Signals.Remove(_signal);
+                GameObject.Destroy(hit.collider.gameObject);
+                GameObject.Destroy(this.gameObject);
             }
         }
+    }
+
+    private void AddReflection(Vector3 startPos, Vector3 endPos)
+    {
+        var reflection = GameObject.Instantiate(ReflectionPrefab);
+        var lineRenderer = reflection.GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+        reflection.transform.parent = gameObject.transform;
+        _reflections.Add(reflection);
     }
 }
