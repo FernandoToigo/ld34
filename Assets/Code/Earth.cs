195596a8b9c14b074ccb671bf90b5db4dbd1a674
Filipe Scur <filipe.scur@gmail.com>
2015-12-12 19:20:33 -0200

    - changed small sat model

---
+++ Assets/Code/Earth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earth : MonoBehaviour {
+    float _t;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _t += 0.1f;
+        this.transform.localRotation = Quaternion.Euler(0.0f, _t, 0.0f);
+    }
+}
