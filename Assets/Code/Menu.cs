ae782f3c8d7c67dc22f67230ebba4994bd3c703a
Filipe Scur <filipe.scur@gmail.com>
2015-12-14 21:08:33 -0200

    - mat

---
+++ Assets/Code/Menu.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Menu : MonoBehaviour {
+
+    public Material SelectedMaterial;
+    public Material Material;
+
+    GameObject playButton;
+    GameObject creditsButton;
+    GameObject exitButton;
+
+    GameObject[] _buttons;
+
+    Action[] _actions;
+
+    // Use this for initialization
+    void Start () {
+        playButton = this.transform.FindChild("PlayButton").gameObject;
+        creditsButton = this.transform.FindChild("CreditsButton").gameObject;
+        exitButton = this.transform.FindChild("ExitButton").gameObject;
+
+        _actions = new Action[]
+            {
+                ()=> {
+                    //GameObject.Find("Satellite").GetComponent<Animator>().SetBool("SatelliteGo", true);
+                    var animation = GameObject.Find("Satellite").GetComponent<Animation>();
+
+                    animation["Intro0"].wrapMode = WrapMode.Once;
+                    animation.Play("Intro0");
+                },
+                ()=> { },
+                ()=> { Application.Quit(); },
+            };
+
+        _buttons = new GameObject[]
+            {
+                playButton,
+                creditsButton,
+                exitButton
+            };
+
+        playButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+        creditsButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+        exitButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+    }
+
+    int _selectedIndex = 0;
+
+    float _lastPressed;
+    float _delay = 0.05f;
+	// Update is called once per frame
+	void Update ()
+    {
+        bool leftArrowPressed = Input.GetKey(KeyCode.LeftArrow);
+        bool rightArrowPressed = Input.GetKey(KeyCode.RightArrow);
+
+        if (leftArrowPressed && rightArrowPressed)
+        {
+            _actions[_selectedIndex]();
+        }
+        else if (leftArrowPressed)
+        {
+            if (_lastPressed < 0.0f)
+            {
+                _selectedIndex--;
+                if (_selectedIndex < 0)
+                    _selectedIndex = 2;
+
+                _lastPressed = _delay;
+            }
+            else
+                _lastPressed -= Time.deltaTime;
+        }
+        else if (rightArrowPressed)
+        {
+            if (_lastPressed < 0.0f)
+            {
+                _selectedIndex++;
+                if (_selectedIndex > 2)
+                    _selectedIndex = 0;
+
+                _lastPressed = _delay;
+            }
+            else
+            {
+                _lastPressed -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            _lastPressed = _delay;
+        }
+        
+        playButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+        creditsButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+        exitButton.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = Material;
+
+        var selected = _buttons[_selectedIndex];
+        selected.transform.FindChild("Model").transform.FindChild("default").GetComponent<MeshRenderer>().material = SelectedMaterial;
+    }
+}
--- Assets/Code/Satelite.cs
+++ Assets/Code/Satelite.cs
@@ -17,12 +17,14 @@ public class Satelite : MonoBehaviour {
     AudioSource _rightTurbineThrusterSound;
     AngleThing _angleThing;
 
+    public bool OnMenu = true;
+
     // Use this for initialization
     void Start()
     {
         _angleThing = GetComponent<AngleThing>();
 
-        this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+        //this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
 
         _rightTurbineThrusterAnimation = this.transform.FindChild("RightTurbine").GetComponent<ParticleSystem>();
         _rightTurbineThrusterAnimation.Stop();
@@ -42,83 +44,89 @@ public class Satelite : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        bool leftArrowPressed = Input.GetKey(KeyCode.LeftArrow);
-        bool rightArrowPressed = Input.GetKey(KeyCode.RightArrow);
-
-        var v0 = Vector3.Normalize(Vector3.Cross(-this.transform.position, new Vector3(0.0f, 0.0f, 1.0f)));
-
-        var percentTranslation = Mathf.Abs(_translationSpeed) / _maxTraslationSpeed;
-        var trueRotationSpeed = RotationSpeedIncrement + percentTranslation;
-
-        if (leftArrowPressed && rightArrowPressed)
+        if (OnMenu)
         {
-            //RotationSpeedIncrement = 0.00f;
-            TranslationSpeedIncrement += 0.005f * Time.deltaTime;
-            _rightTurbineThrusterAnimation.Play();
 
-            if (!_rightTurbineThrusterSound.isPlaying)
-                _rightTurbineThrusterSound.Play();
-
-            _leftTurbineThrusterAnimation.Play();
-
-            if (!_leftTurbineThrusterSound.isPlaying)
-                _leftTurbineThrusterSound.Play();
-        }
-        else if (leftArrowPressed)
-        {
-            TranslationSpeedIncrement = 0.0f;
-            _rotationSpeed = Mathf.Clamp(_rotationSpeed + trueRotationSpeed * Time.deltaTime, -_maxRotationSpeed, _maxRotationSpeed);
-            //RotationSpeedIncrement += 0.015f * Time.deltaTime;
-            _leftTurbineThrusterAnimation.Play();
-
-            if (!_leftTurbineThrusterSound.isPlaying)
-                _leftTurbineThrusterSound.Play();
-
-            _rightTurbineThrusterAnimation.Stop();
-            _rightTurbineThrusterSound.Stop();
-        }
-        else if (rightArrowPressed)
-        {
-            TranslationSpeedIncrement = 0.0f;
-            //RotationSpeedIncrement -= 0.015f * Time.deltaTime;
-            _rotationSpeed = Mathf.Clamp(_rotationSpeed - trueRotationSpeed * Time.deltaTime, -_maxRotationSpeed, _maxRotationSpeed);
-
-            _rightTurbineThrusterAnimation.Play();
-
-            if (!_rightTurbineThrusterSound.isPlaying)
-                _rightTurbineThrusterSound.Play();
-            
-            _leftTurbineThrusterAnimation.Stop();
-            _leftTurbineThrusterSound.Stop();
         }
         else
         {
-            _rightTurbineThrusterAnimation.Stop();
-            _rightTurbineThrusterSound.Stop();
-            _leftTurbineThrusterAnimation.Stop();
-            _leftTurbineThrusterSound.Stop();
-            TranslationSpeedIncrement = 0.0f;
+            bool leftArrowPressed = Input.GetKey(KeyCode.LeftArrow);
+            bool rightArrowPressed = Input.GetKey(KeyCode.RightArrow);
+
+            var v0 = Vector3.Normalize(Vector3.Cross(-this.transform.position, new Vector3(0.0f, 0.0f, 1.0f)));
+
+            var percentTranslation = Mathf.Abs(_translationSpeed) / _maxTraslationSpeed;
+            var trueRotationSpeed = RotationSpeedIncrement + percentTranslation;
+
+            if (leftArrowPressed && rightArrowPressed)
+            {
+                //RotationSpeedIncrement = 0.00f;
+                TranslationSpeedIncrement += 0.005f * Time.deltaTime;
+                _rightTurbineThrusterAnimation.Play();
+
+                if (!_rightTurbineThrusterSound.isPlaying)
+                    _rightTurbineThrusterSound.Play();
+
+                _leftTurbineThrusterAnimation.Play();
+
+                if (!_leftTurbineThrusterSound.isPlaying)
+                    _leftTurbineThrusterSound.Play();
+            }
+            else if (leftArrowPressed)
+            {
+                TranslationSpeedIncrement = 0.0f;
+                _rotationSpeed = Mathf.Clamp(_rotationSpeed + trueRotationSpeed * Time.deltaTime, -_maxRotationSpeed, _maxRotationSpeed);
+                //RotationSpeedIncrement += 0.015f * Time.deltaTime;
+                _leftTurbineThrusterAnimation.Play();
+
+                if (!_leftTurbineThrusterSound.isPlaying)
+                    _leftTurbineThrusterSound.Play();
+
+                _rightTurbineThrusterAnimation.Stop();
+                _rightTurbineThrusterSound.Stop();
+            }
+            else if (rightArrowPressed)
+            {
+                TranslationSpeedIncrement = 0.0f;
+                //RotationSpeedIncrement -= 0.015f * Time.deltaTime;
+                _rotationSpeed = Mathf.Clamp(_rotationSpeed - trueRotationSpeed * Time.deltaTime, -_maxRotationSpeed, _maxRotationSpeed);
+
+                _rightTurbineThrusterAnimation.Play();
+
+                if (!_rightTurbineThrusterSound.isPlaying)
+                    _rightTurbineThrusterSound.Play();
+
+                _leftTurbineThrusterAnimation.Stop();
+                _leftTurbineThrusterSound.Stop();
+            }
+            else
+            {
+                _rightTurbineThrusterAnimation.Stop();
+                _rightTurbineThrusterSound.Stop();
+                _leftTurbineThrusterAnimation.Stop();
+                _leftTurbineThrusterSound.Stop();
+                TranslationSpeedIncrement = 0.0f;
+            }
+
+            //RotationSpeedIncrement = Mathf.Clamp(RotationSpeedIncrement, -0.1f, 0.1f);
+            TranslationSpeedIncrement = Mathf.Clamp(TranslationSpeedIncrement, 0.0f, 0.01f);
+
+            _translationSpeed += TranslationSpeedIncrement * Vector3.Dot(-Vector3.Normalize(this.transform.up), Vector3.Normalize(v0));
+            //_translationSpeed += TranslationSpeedIncrement * Vector3.Dot(-this.transform.up, v0);
+
+            _translationSpeed = Mathf.Clamp(_translationSpeed, -_maxTraslationSpeed, _maxTraslationSpeed);
+
+            //Debug.Log("r:  " + _rotationSpeed + "; t%:  " + _translationSpeed / _maxTraslationSpeed);
+
+            var eulerZ = this.transform.localRotation.eulerAngles.z;
+            this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, eulerZ + _rotationSpeed);
+
+            _angleThing.Angle += Time.deltaTime * _translationSpeed;
+            this.transform.position = _angleThing.Direction * 6.0f;
+
+            //Debug.DrawLine(this.transform.position, this.transform.position - this.transform.up * 1.0f, Color.cyan);
+            //Debug.DrawLine(this.transform.position, new Vector3(), Color.magenta);
+            //Debug.DrawLine(this.transform.position, this.transform.position - v0, Color.green);
         }
-
-        //RotationSpeedIncrement = Mathf.Clamp(RotationSpeedIncrement, -0.1f, 0.1f);
-        TranslationSpeedIncrement = Mathf.Clamp(TranslationSpeedIncrement, 0.0f, 0.01f);
-
-        _translationSpeed += TranslationSpeedIncrement * Vector3.Dot(-Vector3.Normalize(this.transform.up), Vector3.Normalize(v0));
-        //_translationSpeed += TranslationSpeedIncrement * Vector3.Dot(-this.transform.up, v0);
-
-        _translationSpeed = Mathf.Clamp(_translationSpeed, -_maxTraslationSpeed, _maxTraslationSpeed);
-
-        //Debug.Log("r:  " + _rotationSpeed + "; t%:  " + _translationSpeed / _maxTraslationSpeed);
-
-        var eulerZ = this.transform.localRotation.eulerAngles.z;
-        this.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, eulerZ + _rotationSpeed);
-
-        _angleThing.Angle += Time.deltaTime * _translationSpeed;
-        this.transform.position = _angleThing.Direction * 6.0f;
-
-        //Debug.DrawLine(this.transform.position, this.transform.position - this.transform.up * 1.0f, Color.cyan);
-        //Debug.DrawLine(this.transform.position, new Vector3(), Color.magenta);
-        //Debug.DrawLine(this.transform.position, this.transform.position - v0, Color.green);
-
     }
 }
